e507e7129e92a42b7371527b189ff0709b4d082b
Amber Robinson <ballewaer@gmail.com>
2019-12-12 22:22:21 -0800

    Student Lambdasian

--- index.js
+++ index.js
@@ -170,8 +170,22 @@ class Instructor extends Lambdasian{
         + `PRAssignment` a method that receives a subject as an argument and returns `student.name has submitted a PR for {subject}`
         + `sprintChallenge` similar to PRAssignment but returns `student.name has begun sprint challenge on {subject}`
 */
-class Student {
-
+class Student extends Lambdasian{
+  constructor(stuAttr) {
+    super(stuAttr);
+    this.previousBackground = stuAttr.previousBackground;
+    this.className = stuAttr.className;
+    this.favSubjects = stuAttr.favSubjects;
+  }
+    listSubjects() {
+      return `Loving ${this.favSubjects}!`;
+    }
+    PRAssignment(subject) {
+      return `${this.name} has submitted a PR for ${subject}`;
+    }
+    sprintChallenge(subject) {
+      return `${this.name} has beun sprint challenge on ${subject}`;
+    }
 }
 
 /*
